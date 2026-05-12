ba8536cb0bdfaa74d9f9d70ae7048191034d4153
Akash0-0 <palkeshu2604@gmail.com>
2026-05-12 23:40:56 +0530

    Add files via upload

--- history_aware_generation.py
+++ history_aware_generation.py
@@ -15,7 +15,7 @@ db = Chroma(persist_directory=persistent_directory, embedding_function=embedding
 
 # Set up AI model
 model = ChatOpenAI(
-    model="openrouter/free",
+    model="gryphe/mythomist-7b",
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY")
 )
